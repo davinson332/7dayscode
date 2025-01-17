e0437673ddb11db881901e3e5b3b25879164fc1c
davinson moreno <davinsonabmoreno@gmail.com>
2025-01-17 17:45:10 -0500

    Add files via upload

---
+++ 7code ejercicio1/app.js
@@ -0,0 +1,34 @@
+// Declaracion de variables
+let numeroUn = 1;
+let stringUn = "1";
+let numeroTreinta = 30;
+let stringTreinta = "30";
+let numeroDiez = 10;
+let stringDiez = "10";
+
+// Comparacion numeroUN y stringUn
+if (numeroUn == stringUn) {
+    console.log("Las variables numeroUn y stringUn tienen el mismo valor y el mismo tipo.");
+} else {
+    console.log("Las variables numeroUn y stringUn no tienen el mismo valor.");
+}
+
+
+// Comparacion entere numeroTreinta y stringTreinta 
+if (numeroTreinta === stringTreinta) {
+    console.log("Las variuables nuemeroTreinta y stringTreinta tienen el mismo valor y el mismo tipo.");
+}else {
+    console.log("Las variables numerTreinta y stringTreinta no tienen el mismo valor.");
+}
+
+//Comparacion entre numeroDiez y stringDiez
+if (numeroDiez == stringDiez) {
+    console.log("Las variables numeroDiez y stringDiez tienen el mismo valor y el mismo tipo.");
+}else {
+    console.log("Las variabales numeroDiez y stringDiez no tienen el mismo valor.")
+}
+
+
+
+
+
